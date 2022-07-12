f419a5f1a042973a51b91e1ee1f1baea36ac59dc
Shahbaz <shbz8888@gmail.com>
2022-07-12 19:33:16 +0100

    wow

---
+++ scraper.py
@@ -0,0 +1,39 @@
+# gamma
+import selenium
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+driver = webdriver.Chrome()
+
+driver.get("https://gorillamind.com/")
+
+
+
+
+def load_and_accept_cookies() -> webdriver.Chrome:
+    '''
+    Open Zoopla and accept the cookies
+    
+    Returns
+    -------
+    driver: webdriver.Chrome
+        This driver is already in the Zoopla webpage
+    '''
+    driver = webdriver.Chrome() 
+    driver.get("https://gorillamind.com/")
+    link = driver.find_element(By.LINK_TEXT,"All Products")
+    link.click()
+    time.sleep(10)
+    modal = driver.find_element(By.XPATH,'//button[@class="sc-75msgg-0 RlRPc close-button cw-close"]')
+    modal.click()
+    
+    
+load_and_accept_cookies()
+
+
+
+
+    
