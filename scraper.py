a2cabe8be508aec0cdcfd1df1516617f4835abf6
Shahbaz <shbz8888@gmail.com>
2022-07-14 17:23:37 +0100

    successful extraction of product link and page naviagtion

--- scraper.py
+++ scraper.py
@@ -8,34 +8,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 
+driver.get("https://gorillamind.com/")
+time.sleep(1)
+link = driver.find_element(By.LINK_TEXT,"All Products")
+time.sleep(1)
+link.click()
+time.sleep(10)
 
-
-def open_website()-> webdriver.Chrome:
-    '''
-    Open Zoopla and accept the cookies
-    
-    Returns
-    -------
-    driver: webdriver.Chrome
-        This driver is already in the Zoopla webpage
-    '''
-    driver = webdriver.Chrome() 
-    driver.get("https://gorillamind.com/")
-    time.sleep(3)
-    link = driver.find_element(By.LINK_TEXT,"All Products")
-    link.click()
-    time.sleep(10)
-    try:
+try: 
         modal = driver.find_element(By.XPATH,'//button[@class="sc-75msgg-0 RlRPc close-button cw-close"]')
+        print(modal)
         modal.click()
-        time.sleep(3)
-    except:
-        time.sleep(3)
-        pass
-       
-open_website()
+        print('Button clicked')    
+
+except:
+        print('No button found...exiting')
+        driver.quit() 
 
 
-
-
-    
+page_1 = driver.find_element(By.XPATH, '//a[@class="hidden-product-link"]')
+#a_tag = page_1.find_element(By.TAG_NAME,'a')
+link = page_1.get_attribute('href')
+print(link)
