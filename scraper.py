5ff99257330f11a68ddc7879688ecfd0eaa55dad
Shahbaz <shbz8888@gmail.com>
2022-07-15 17:57:47 +0100

    extracted product links from page 1 and 2

--- scraper.py
+++ scraper.py
@@ -10,9 +10,9 @@ driver = webdriver.Chrome()
 
 driver.get("https://gorillamind.com/")
 time.sleep(1)
-link = driver.find_element(By.LINK_TEXT,"All Products")
+AllProducts = driver.find_element(By.LINK_TEXT,"All Products")
 time.sleep(1)
-link.click()
+AllProducts.click()
 time.sleep(10)
 
 try: 
@@ -34,6 +34,17 @@ gear_link_list = []
 for gear in gear_list:
         link = gear.get_attribute('href')
         gear_link_list.append(link)
+time.sleep(1)        
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+next = driver.find_element(By.LINK_TEXT,"Next")
+next.click()
+time.sleep(2) 
+
+gear_container2 = driver.find_element(By.XPATH, '//div[@class="container collection-matrix"]')
+gear_list2 = gear_container2.find_elements(By.XPATH, './/a[@class="hidden-product-link"]')
+for gear2 in gear_list2:
+        link = gear2.get_attribute('href')
+        gear_link_list.append(link)
 
 print(f'There are {len(gear_link_list)} products in this page')
-print(gear_link_list)        
+print(gear_link_list)
