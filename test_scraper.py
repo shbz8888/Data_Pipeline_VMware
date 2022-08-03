f9a33ef71c574386d6bfa6d9be173cf13f2db3e7
Shahbaz <shbz8888@gmail.com>
2022-08-03 18:23:14 +0100

    first unittest complete

--- test_scraper.py
+++ test_scraper.py
@@ -1,6 +1,21 @@
 import unittest
 from Project.scraper import Scraper 
 
-class TestScraper(unittest.Testcase):
+class ScraperTestCase(unittest.TestCase):
 
-    def test_
+
+      
+
+
+    def test_create_dict(self):
+        name = "GORILLA MODE"
+        price = "$49.99"
+        description = "\u2022 Intense Focus + Drive*\n\u2022 Huge Nitric Oxide Pump*\n\u2022 Increased Endurance + Power*"
+        size = "40 Servings"
+        num_reviews = "1521 Reviews"
+        strID = "b0c08216-36ab-42cf-8c4d-59c9a22486d6"
+        final_image_link = "https://cdn.shopify.com/s/files/1/0369/2580/0493/products/Gorilla-Mode-OG-Volcano-Burst_5000x.png?v=1656857311"
+        B = Scraper.create_dict(self,name, price, description, size, num_reviews, strID, final_image_link)
+        A = {'Name': ["GORILLA MODE"], 'Price': ["$49.99"], 'Description': ["\u2022 Intense Focus + Drive*\n\u2022 Huge Nitric Oxide Pump*\n\u2022 Increased Endurance + Power*"], 'Size': ["40 Servings"], 'Num_reviews': ["1521 Reviews"], 'UUID': ["b0c08216-36ab-42cf-8c4d-59c9a22486d6"], 'Image': ["https://cdn.shopify.com/s/files/1/0369/2580/0493/products/Gorilla-Mode-OG-Volcano-Burst_5000x.png?v=1656857311"]}
+        self.assertDictEqual(A,B)
+
