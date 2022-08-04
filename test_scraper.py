84fc240444290379333078ffb75884646d4320bd
Shahbaz <shbz8888@gmail.com>
2022-08-04 11:43:04 +0100

    test implemented for 3 methods

--- test_scraper.py
+++ test_scraper.py
@@ -19,9 +19,20 @@ class ScraperTestCase(unittest.TestCase):
     Scraper.__init__(unittest.TestCase)
 
     Scraper.visit_individual_link(unittest.TestCase)
-    Scraper.extract_text(unittest.TestCase)  
-    Scraper.extract_image(unittest.TestCase)  
+    time.sleep(10)
+    Scraper.__close_modal__(unittest.TestCase)
+
+    final_image_link, final_image = Scraper.extract_image(unittest.TestCase)  
     
+    def test_extract_text(self):
+        name, price, description, size, num_reviews, strID = Scraper.extract_text(unittest.TestCase) 
+        self.assertEqual(num_reviews,"NaN")
+        self.assertEqual(len(strID),36)
+
+    def test_extract_image(self):
+        final_image_link, final_image = Scraper.extract_image(unittest.TestCase)  
+        self.assertTrue(type(final_image_link) is str)
+        self.assertEqual((len(final_image_link)-len(final_image)),6)
 
 
 
@@ -35,9 +46,9 @@ class ScraperTestCase(unittest.TestCase):
         num_reviews = "1521 Reviews"
         strID = "b0c08216-36ab-42cf-8c4d-59c9a22486d6"
         final_image_link = "https://cdn.shopify.com/s/files/1/0369/2580/0493/products/Gorilla-Mode-OG-Volcano-Burst_5000x.png?v=1656857311"
-        B = Scraper.create_dict(self, name, price, description, size, num_reviews, strID, final_image_link)
+        D = Scraper.create_dict(self, name, price, description, size, num_reviews, strID, final_image_link)
         A = {'Name': ["GORILLA MODE"], 'Price': ["$49.99"], 'Description': ["\u2022 Intense Focus + Drive*\n\u2022 Huge Nitric Oxide Pump*\n\u2022 Increased Endurance + Power*"], 'Size': ["40 Servings"], 'Num_reviews': ["1521 Reviews"], 'UUID': ["b0c08216-36ab-42cf-8c4d-59c9a22486d6"], 'Image': ["https://cdn.shopify.com/s/files/1/0369/2580/0493/products/Gorilla-Mode-OG-Volcano-Burst_5000x.png?v=1656857311"]}
-        self.assertDictEqual(A,B)
+        
         self.assertTrue(type(name) is str)
 
     
